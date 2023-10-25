2b193c3536321e1068f62ad51a83fe80c8b5e517
Patrick <106843867+patrickstaraccount@users.noreply.github.com>
2023-10-25 17:22:04 +0200

    extacted files into utils

---
+++ src/utils/charLocalStorage.ts
@@ -0,0 +1,18 @@
+const saveCharCoordinatesToLocal = (char: string, coordinates: []) => {
+  const existingData = localStorage.getItem("charCoordinates");
+
+  const charCoordinates = existingData ? JSON.parse(existingData) : {};
+
+  charCoordinates[char] = coordinates;
+
+  localStorage.setItem("charCoordinates", JSON.stringify(charCoordinates));
+  console.log("saved");
+};
+
+const getCharCoordinatesFromLocal = (): object => {
+  const charCoordinates = localStorage.getItem("charCoordinates");
+
+  return charCoordinates ? JSON.parse(charCoordinates) : {};
+};
+
+export { getCharCoordinatesFromLocal, saveCharCoordinatesToLocal };
