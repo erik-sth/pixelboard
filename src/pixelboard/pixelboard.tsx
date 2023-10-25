fab2614d43d7dbe2ef99f72f0803f4668ae8eefe
Patrick <106843867+patrickstaraccount@users.noreply.github.com>
2023-10-25 15:26:10 +0200

    prototype1 of car mapping

--- src/pixelboard/pixelboard.tsx
+++ src/pixelboard/pixelboard.tsx
@@ -1,41 +1,239 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-interface MatrixComponentProps {
-  initialHeight: number;
-  initialWidth: number;
-}
+const matrixSize = { height: 5, width: 19 };
 
-const MatrixComponent: React.FC<MatrixComponentProps> = ({
-  initialHeight,
-  initialWidth,
-}) => {
-  const initialMatrix: boolean[][] = Array.from({ length: initialHeight }, () =>
-    Array(initialWidth).fill(false)
-  );
+const charToCoordinates = {
+  A: [
+    [0, 1, 0],
+    [1, 0, 1],
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+  ],
+  B: [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+    [1, 1, 1],
+  ],
+  C: [
+    [0, 1, 1],
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0],
+    [0, 1, 1],
+  ],
+  D: [
+    [1, 1, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 1, 0],
+  ],
+  E: [
+    [1, 1, 1],
+    [1, 0, 0],
+    [1, 1, 0],
+    [1, 0, 0],
+    [1, 1, 1],
+  ],
+  F: [
+    [1, 1, 1],
+    [1, 0, 0],
+    [1, 1, 0],
+    [1, 0, 0],
+    [1, 0, 0],
+  ],
+  G: [
+    [0, 1, 1],
+    [1, 0, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 1],
+  ],
+  H: [
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+  ],
+  I: [[1], [1], [1], [1], [1]],
+  J: [
+    [0, 0, 1],
+    [0, 0, 1],
+    [0, 0, 1],
+    [1, 0, 1],
+    [0, 1, 0],
+  ],
+  K: [
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+  ],
+  L: [
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 1, 1],
+  ],
+  M: [
+    [1, 0, 0, 0, 1],
+    [1, 1, 0, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+  ],
+  N: [
+    [1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 0, 1, 1],
+    [1, 0, 0, 0, 1],
+  ],
+  O: [
+    [0, 1, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 0],
+  ],
+  P: [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+    [1, 0, 0],
+    [1, 0, 0],
+  ],
+  Q: [
+    [0, 1, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 1],
+    [0, 0, 1],
+  ],
+  R: [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 0],
+  ],
+  T: [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ],
+  U: [
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 0],
+  ],
+  V: [
+    [1, 0, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 0],
+    [0, 1, 0],
+  ],
+  W: [
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 1],
+    [1, 0, 0, 0, 1],
+  ],
+  X: [
+    [1, 0, 0, 0, 1],
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+  ],
+  Y: [
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ],
+  Z: [
+    [1, 0, 0, 0, 1],
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+  ],
+};
 
-  const [matrix, setMatrix] = useState<boolean[][]>(initialMatrix);
+const MatrixComponent = () => {
+  const [matrix, setMatrix] = useState(() => {
+    const initialMatrix = Array.from({ length: matrixSize.height }, () =>
+      Array(matrixSize.width).fill(false)
+    );
+    return initialMatrix;
+  });
 
-  const toggleCell = (row: number, col: number): void => {
-    const newMatrix = [...matrix];
-    newMatrix[row][col] = !newMatrix[row][col];
+  const updateMatrixFromText = (inputText: String) => {
+    const newMatrix = Array.from({ length: matrixSize.height }, () =>
+      Array(matrixSize.width).fill(false)
+    );
+
+    let currentXPosition = 0;
+
+    for (let i = 0; i < inputText.length; i++) {
+      const char = inputText[i].toUpperCase();
+      const coordinates = charToCoordinates[char];
+      console.log(coordinates);
+
+      if (coordinates) {
+        for (let j = 0; j < coordinates.length; j++) {
+          for (let k = 0; k < coordinates[j].length; k++) {
+            newMatrix[j][k + currentXPosition] = coordinates[j][k];
+          }
+        }
+      }
+
+      currentXPosition += coordinates[0].length + 1;
+    }
+
     setMatrix(newMatrix);
   };
 
   return (
     <div>
+      <textarea
+        placeholder="Enter text"
+        onBlur={(e) => updateMatrixFromText(e.target.value)}
+      />
+
       {matrix.map((row, rowIndex) => (
         <div key={rowIndex}>
           {row.map((cell, colIndex) => (
             <span
               key={colIndex}
-              onClick={() => toggleCell(rowIndex, colIndex)}
               style={{
-                display: 'inline-block',
-                width: '20px',
-                height: '20px',
-                border: '1px solid #000',
-                background: cell ? 'black' : 'white', 
-                cursor: 'pointer',
+                display: "inline-block",
+                width: "20px",
+                height: "20px",
+                border: "1px solid #000",
+                background: cell ? "black" : "white",
               }}
             />
           ))}
@@ -46,4 +244,3 @@ const MatrixComponent: React.FC<MatrixComponentProps> = ({
 };
 
 export default MatrixComponent;
-
