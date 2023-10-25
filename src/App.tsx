5a719a38ae69660a55353de8a35ef564cf8bc4e6
Patrick <106843867+patrickstaraccount@users.noreply.github.com>
2023-10-25 16:14:53 +0200

    Revert "Added saveToLocalStorage add getFromLocalStorage."
    This reverts commit 04325f2a883c809b13e965545f430c8c3edf3906.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import MatrixComponent from './pixelboard/pixelboard';
-import { useState } from 'react';
+import MatrixComponent from "./pixelboard/pixelboard";
+import { useState } from "react";
 
 function App() {
-	const [currentString, setCurrentString] = useState<string>('');
-	const [allWords, setAllWords] = useState<String[]>([]);
+  const [currentString, setCurrentString] = useState<string>("");
+  const [allWords, setAllWords] = useState<String[]>([]);
 
-	return (
-		<div>
-			<MatrixComponent
-				width={20}
-				height={5}
-				word={currentString}
-			></MatrixComponent>
-			<textarea
-				placeholder='Enter text'
-				onBlur={(e) => setAllWords([...allWords, e.target.value])}
-			/>
-			<ul>
-				{allWords.map((word, index) => (
-					<li key={index}>
-						<button onClick={() => setCurrentString(word)}>
-							{word}
-						</button>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <MatrixComponent
+        width={20}
+        height={5}
+        word={currentString}
+      ></MatrixComponent>
+      <textarea
+        placeholder="Enter text"
+        onBlur={(e) => setAllWords([...allWords, e.target.value])}
+      />
+      <ul>
+        {allWords.map((word, index) => (
+          <li key={index}>
+            <button onClick={() => setCurrentString(word)}>{word}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
